542df8c20f2a6021bb60a0fa1d1a73307558c902
SadieZhang <517502246@qq.com>
2020-02-12 10:17:23 +0800

    2.0

--- src/main/java/com/pipeline/testPipeline/HelloController.java
+++ src/main/java/com/pipeline/testPipeline/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World! 2.0";
     }
 }
